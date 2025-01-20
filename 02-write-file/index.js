b05bb09e4599d20feee52926efbc117414e7646d
Anna Sadovaia <lady.sadovaya@gmail.com>
2025-01-20 22:26:10 +0500

    feat: add the solution for second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,27 @@
-console.log('Hi! Please input anything:');
 const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Hi! Please input anything (for exit type "exit"):');
 
 rl.on('line', (input) => {
-  let data = input;
-  fs.writeFile('./02-write-file/text.txt', data);
-})
+  if (input.trim().toLowerCase() === 'exit') {
+    rl.close();
+  } else {
+    fs.appendFile('./02-write-file/text.txt', input + '\n', (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
+rl.on('SIGINT', () => {
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('Goodbye!');
+});
